edb3dc47e82a1429a8bb1476b4c050d8bc043ac8
Victor <victorribera92@gmail.com>
2019-06-29 12:24:59 +0200

    carousel pics

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,10 +3,9 @@
     <!-- NAVBAR -->
 
     <div class="test">
-      <v-carousel delimiter-icon="stop" class="carouselDiv">
+      <v-carousel hide-delimiters class="carouselDiv">
         <v-carousel-item class="carouselImage" v-for="(item,i) in items" :key="i" :src="item.src"></v-carousel-item>
       </v-carousel>
-      <p>tessdfsfdsdfst</p>
     </div>
   </div>
 </template>
@@ -18,19 +17,52 @@ export default {
     return {
       items: [
         {
-          src: require("@/assets/didacmarc.jpg")
+          src: require("@/assets/victorferran.jpg")
         },
         {
-          src: require("@/assets/equipo.jpg")
-        },
-        {
-          src: require("@/assets/graduacion1.jpg")
-        },
-        {
-          src: require("@/assets/ferranpodio.jpg")
+          src: require("@/assets/cesar.jpg")
         },
         {
           src: require("@/assets/ferranainoa.jpg")
+        },
+        {
+          src: require("@/assets/didacmarc1.jpg")
+        },
+        {
+          src: require("@/assets/victor2.jpg")
+        },
+        {
+          src: require("@/assets/marc.jpg")
+        },
+        {
+          src: require("@/assets/ainoa1.jpg")
+        },
+        {
+          src: require("@/assets/marsal.jpg")
+        },
+        {
+          src: require("@/assets/mestrevictor.jpg")
+        },
+        {
+          src: require("@/assets/mestre.jpg")
+        },
+        {
+          src: require("@/assets/pacouri.jpg")
+        },
+        {
+          src: require("@/assets/kevin.jpg")
+        },
+        {
+          src: require("@/assets/pedro.jpg")
+        },
+        {
+          src: require("@/assets/polaida.jpg")
+        },
+        {
+          src: require("@/assets/victordavid.jpg")
+        },
+        {
+          src: require("@/assets/victor1.jpg")
         }
       ]
     };
